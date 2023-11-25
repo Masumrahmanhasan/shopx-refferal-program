01d514447431ff93e75ac162e96aad7d113dd6ea
masumrahmanhasan <masumrahmanhasan@gmail.com>
2023-11-25 12:43:36 +0600

    nagad copy to clipboard bug issue fix

--- resources/views/livewire/payment-section.blade.php
+++ resources/views/livewire/payment-section.blade.php
@@ -81,7 +81,7 @@
                 <h5
                     class="flex gap-3 items-center justify-center bg-[#232323] p-5 mt-4 rounded-xl text-2xl text-white">
                     {{ get_settings('nagad') }}
-                    <button wire:click="copyToClipboard({{ get_settings('nagad') }})">
+                    <button wire:click="copyToClipboard('{{ get_settings('nagad') }}')">
                         <img src="/images/copy.png" alt="">
                     </button>
                 </h5>
