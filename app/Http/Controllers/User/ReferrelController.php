a16215f9e5ee2ae040b92aa33d0598c66fccbff9
masumrahmanhasan <masumrahmanhasan@gmail.com>
2024-01-07 01:59:18 +0600

    count fixed

--- app/Http/Controllers/User/ReferrelController.php
+++ app/Http/Controllers/User/ReferrelController.php
@@ -13,28 +13,28 @@ class ReferrelController extends Controller
             ->with('associate')
             ->where('user_id', auth()->id())
             ->get();
-        $data = UserReferrel::query()
-            ->where('user_id', auth()->id())
-            ->join('users', 'users.id', '=', 'user_referrels.user_id')
-            ->selectRaw('COALESCE(sum(CASE WHEN generation = "first" AND users.status = "active" THEN 1 ELSE 0 END), 0) as first_active')
-            ->selectRaw('COALESCE(sum(CASE WHEN generation = "first" AND users.status = "inactive" THEN 1 ELSE 0 END), 0) as first_inactive')
-            ->selectRaw('COALESCE(sum(CASE WHEN generation = "second" AND users.status = "active" THEN 1 ELSE 0 END), 0) as second_active')
-            ->selectRaw('COALESCE(sum(CASE WHEN generation = "second" AND users.status = "inactive" THEN 1 ELSE 0 END), 0) as second_inactive')
-            ->selectRaw('COALESCE(sum(CASE WHEN generation = "third" AND users.status = "active" THEN 1 ELSE 0 END), 0) as third_active')
-            ->selectRaw('COALESCE(sum(CASE WHEN generation = "third" AND users.status = "inactive" THEN 1 ELSE 0 END), 0) as third_inactive')
-            ->groupBy('user_id')
-            ->first();
 
-        if (!$data) {
-            $data = (object)[
-                'first_active' => 0,
-                'first_inactive' => 0,
-                'second_active' => 0,
-                'second_inactive' => 0,
-                'third_active' => 0,
-                'third_inactive' => 0,
-            ];
-        }
+        $user_id = auth()->id();
+        $generations = ['first', 'second', 'third'];
+        $statuses = ['active', 'inactive'];
+
+        $data = collect($generations)
+            ->crossJoin($statuses)
+            ->flatMap(function ($combination) use ($user_id) {
+                list($generation, $status) = $combination;
+
+                $key = "{$generation}_{$status}";
+
+                return [$key => UserReferrel::query()->where('user_id', $user_id)
+                    ->where('generation', $generation)
+                    ->whereHas('associate', function ($query) use ($status) {
+                        $query->where('status', $status);
+                    })
+                    ->count()];
+            })->toArray();
+
+        $data = (object)$data;
+
         return view('referrel', compact('referrals', 'data'));
     }
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -10,6 +10,7 @@ use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Relations\HasOne;
 use Illuminate\Database\Eloquent\Relations\MorphOne;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Lab404\Impersonate\Models\Impersonate;
@@ -26,7 +27,7 @@ use Laravel\Sanctum\HasApiTokens;
  */
 class User extends Authenticatable
 {
-    use HasApiTokens, HasFactory, Notifiable, Impersonate;
+    use HasApiTokens, HasFactory, Notifiable, Impersonate, SoftDeletes;
 
     /**
      * The attributes that are mass assignable.
